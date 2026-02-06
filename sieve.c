8aeed710ddb2e97f265ef44b512f6c81ba810c87
zakcole08 <zakcole08@gmail.com>
2026-02-06 11:11:27 +0000

    Add first random noise algorithm for image generation

--- sieve.c
+++ sieve.c
@@ -21,9 +21,9 @@ void print_usage() {
 
 void draw_p3() {
 	
+	SDL_Rect pixel = (SDL_Rect){x, y, pix_h, pix_w};
 	for (int y = 0; y < win_h; y++) {
 		for (int x = 0; x < win_w; x++) {			
-			SDL_Rect pixel = (SDL_Rect){x, y, pix_h, pix_w};
 			fscanf(pfile, "%hhd %hhd %hhd", &r, &g, &b);
 			colour = SDL_MapRGB(psurface->format, r, g, b);
 			pixel.x = x;
@@ -48,37 +48,7 @@ void draw_p6() {
 	}
 }
 
-void launch_home(int win_w, int win_h) {	
-	pwindow = SDL_CreateWindow(
-		title,
-		SDL_WINDOWPOS_CENTERED,
-		SDL_WINDOWPOS_CENTERED,
-		win_w,
-		win_h,
-		0
-	);
-	psurface = SDL_GetWindowSurface(pwindow);
-
-	r = 0xB5;
-	g = 0x7E;
-	b = 0xDC;
-	SDL_Rect pixel = (SDL_Rect){x, y, pix_h, pix_w};
-	colour = SDL_MapRGB(psurface->format, r, g, b);
-	pixel.x = x;
-	pixel.y = y;
-	SDL_FillRect(psurface, &pixel, colour);
-	SDL_UpdateWindowSurface(pwindow);
-	SDL_Delay(3000);
-	exit(0);
-}
-
 void launch_window(FILE *pfile) {
-	if (pfile == NULL) {
-		win_w = 800;
-		win_h = 500;
-		launch_home(win_w, win_h);
-	}
-
 	char *pline = calloc(1000, sizeof(char));
 	char *pdimensions = calloc(1000, sizeof(char));
 	int counter = 0;
@@ -151,6 +121,66 @@ void launch_window(FILE *pfile) {
 	}
 }
 
+void launch_home(int win_w, int win_h) {	
+	pwindow = SDL_CreateWindow(
+		title,
+		SDL_WINDOWPOS_CENTERED,
+		SDL_WINDOWPOS_CENTERED,
+		win_w,
+		win_h,
+		0
+	);
+	psurface = SDL_GetWindowSurface(pwindow);
+
+	Uint8 rmin, rmax, gmin, gmax, bmin, bmax;
+	r = (Uint8) rand();
+	g = (Uint8) rand();
+	b = (Uint8) rand();
+	SDL_Rect pixel = (SDL_Rect){x, y, pix_h, pix_w};
+	for (int y = 0; y < win_h; y++) {
+		for (int x = 0; x < win_w; x++) {			
+			rmin = r - 1;
+			rmax = r + 1;
+			gmin = g - 1;
+			gmax = g + 1;
+			bmin = b - 1;
+			bmax = b + 1;
+			r = rand() % (rmax - rmin + 1) + rmin;
+			g = rand() % (gmax - gmin + 1) + gmin;
+			b = rand() % (bmax - bmin + 1) + bmin;
+			colour = SDL_MapRGB(psurface->format, r, g, b);
+			pixel.x = x;
+			pixel.y = y;
+			SDL_FillRect(psurface, &pixel, colour);
+		}
+	}
+
+	SDL_UpdateWindowSurface(pwindow);
+	
+	SDL_Event event;
+	SDL_EventState(SDL_DROPFILE, SDL_ENABLE);
+
+	while (true) {
+		while (SDL_PollEvent(&event)) {
+			switch (event.type) {
+				case SDL_QUIT:
+					SDL_DestroyWindow(pwindow);
+					exit(0);
+
+				case SDL_DROPFILE:
+					pfile = fopen(event.drop.file, "r");
+					SDL_free(event.drop.file);
+
+					SDL_DestroyWindow(pwindow);
+					launch_window(pfile);
+					break;
+				default:
+					break;
+			}
+		}
+	}
+}
+
 int main(int argc, char *argv[]) {
 	if (argc > 1) {
 		pfile = fopen(argv[1], "r");
@@ -160,7 +190,9 @@ int main(int argc, char *argv[]) {
 			return 1;
 		}
 	} else {
-		pfile = NULL;
+		win_w = 1200;
+		win_h = 800;
+		launch_home(win_w, win_h);
 	}
 	launch_window(pfile);
 }
